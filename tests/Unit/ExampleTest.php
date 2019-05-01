65e64e82550f18cf8236cf95bd238f5dc14913ed
mesadhan <sadhansarker3@gmail.com>
2019-05-01 12:24:24 +0600

    use memory database when run test configuration

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,16 +2,16 @@
 
 namespace Tests\Unit;
 
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
+    // to use memory database when run test, jus uncomment `phpunit.xml` configuration
+    use DatabaseMigrations;
+
+
     public function testBasicTest()
     {
         $this->assertTrue(true);
