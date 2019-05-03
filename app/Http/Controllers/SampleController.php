61541a82522204347e470dbcc34cb427d451414f
mesadhan <sadhansarker3@gmail.com>
2019-05-03 22:20:41 +0600

    create sample item for quick start

---
+++ app/Http/Controllers/SampleController.php
@@ -0,0 +1,34 @@
+<?php
+
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SampleController extends Controller
+{
+
+    public function index()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function delete($id)
+    {
+        //
+    }
+}
---
+++ database/factories/SampleFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+
+use App\Http\Models\SampleModel;
+use Faker\Generator as Faker;
+
+$factory->define(SampleModel::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
